3c4c53a36bf494c48e8a89660ccf692d29e7cebd
Jesper Wrang <jeswr740@student.liu.se>
2019-12-05 11:09:33 +0100

    Fixed some spelling mistakes on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -37,7 +37,7 @@ const About = props => {
         <h2 className='todo'>FAQ</h2>
         <b className='question'>Q: I posted some sensitive information on Reddit. Can you delete this from your page?</b>
         <p>
-          No, I can't remove anything myself since I am not the not the one storing all the deleted comment.
+          No, I can't remove anything myself since I am not the not the one storing all the deleted comments.
           This is done by an external service run by Jason Baumgartner (<a href='https://www.reddit.com/user/Stuck_In_the_Matrix'>/u/Stuck_In_the_Matrix</a>).
           If you want something sensitive removed permanently you should contact him.
         </p>
@@ -46,21 +46,21 @@ const About = props => {
           This page is only possible because of the amzaing work done by Jason.
           His site <a href='https://pushshift.io/'>pushshift.io</a> activly listens for new comments on reddit and stores them in his own database.
           Then sites like removeddit and ceddit can fetch these comment from pushshift.
-          Removeddit know what comment reddit shows (from Reddits API) and what comment should be showed (from Pushshift).
+          Removeddit know what comment reddit shows (from Reddits API) and what comment should be showed (from Pushshifts API).
           By comparing the comments from these 2 APIs, we can figure out what has been deleted and removed.
         </p>
         <b className='question'>Q: What's the difference between ceddit and removeddit?</b>
         <p>
           Not much. Removeddit was created as a temporary replacement for ceddit, at a time when ceddit didn't work.
-          I though this was necessary since I used ceddit more then Reddit itself.
+          I thought this was necessary since I used ceddit more then Reddit itself.
           Months later ceddit was fixed, but I didn't see any reason to remove what I had done.
           Today both sites live side by side and strive for the same goal.
         </p>
         <p>
-          There are some minor differences in them tho:
+          There are some minor differences in them though:
           <ul>
             <li>
-              Ceddit respect user deletion while removeddit does not. This decision was made early on and I feel like it's too late to change now.
+              Ceddit respect user made deletions while removeddit does not. This decision was made early on and I feel like it's too late to change now.
               If I had created removeddit today I might had thought more about what was right here.
             </li>
             <li>
@@ -71,7 +71,6 @@ const About = props => {
               Ceddit provides user lookup while removeddit doesn't.
             </li>
           </ul>
-
         </p>
 
         <h2 className='contact'>Links/Contact</h2>
