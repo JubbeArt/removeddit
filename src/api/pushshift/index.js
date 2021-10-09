663e0c9618091afeea448dbc3d7332090358f884
ayan4m1 <andrew@bulletlogic.com>
2021-10-09 12:23:23 -0700

    rewrite pushshift wrapper to use api.pushshift.com instead of elastic.pushshift.com

--- src/api/pushshift/index.js
+++ src/api/pushshift/index.js
@@ -1,57 +1,30 @@
-import { toBase10, toBase36 } from '../../utils'
+import { toBase36 } from '../../utils'
 
-const postURL = 'https://elastic.pushshift.io/rs/submissions/_search?source='
-const commentURL = 'https://elastic.pushshift.io/rc/comments/_search?source='
+const postURL = 'https://api.pushshift.io/reddit/search/submission'
+const commentsURL = 'https://api.pushshift.io/reddit/submission/comment_ids/'
+const commentURL = 'https://api.pushshift.io/reddit/search/comment'
 
-export const getPost = threadID => {
-  const elasticQuery = {
-    query: {
-      term: {
-        id: toBase10(threadID)
-      }
-    }
-  }
+const fetchJson = (url) => window.fetch(url).then((response) => response.json())
 
-  return window.fetch(postURL + JSON.stringify(elasticQuery))
-    .then(response => response.json())
-    .then(response => {
-      const post = response.hits.hits[0]._source
-      post.id = toBase36(post.id)
-      return post
+export const getPost = (threadID) =>
+  fetchJson(`${postURL}?ids=${threadID}&size=1`)
+    .then(([result]) => ({
+      ...result,
+      id: toBase36(result.id),
+    }))
+    .catch(() => {
+      throw new Error('Could not get removed post')
     })
-    .catch(() => { throw new Error('Could not get removed post') })
-}
 
-export const getComments = threadID => {
-  const elasticQuery = {
-    query: {
-      match: {
-        link_id: toBase10(threadID)
-      }
-    },
-    size: 20000,
-    _source: [
-      'author', 'body', 'created_utc', 'parent_id', 'score', 'subreddit', 'link_id'
-    ]
-  }
-
-  return window.fetch(commentURL + JSON.stringify(elasticQuery))
-    .then(response => response.json())
-    .then(response => {
-      const comments = response.hits.hits
-      return comments.map(comment => {
-        comment._source.id = toBase36(comment._id)
-        comment._source.link_id = toBase36(comment._source.link_id)
-
-        // Missing parent id === direct reply to thread
-        if (!comment._source.parent_id) {
-          comment._source.parent_id = threadID
-        } else {
-          comment._source.parent_id = toBase36(comment._source.parent_id)
-        }
-
-        return comment._source
-      })
+export const getComments = (threadID) =>
+  fetchJson(`${commentsURL}${threadID}`)
+    .then(({ data }) => fetchJson(`${commentURL}?ids=${data.join(',')}`))
+    .then(({ data }) =>
+      data.map((comment) => ({
+        ...comment,
+        parent_id: comment.parent_id || threadID,
+      }))
+    )
+    .catch(() => {
+      throw new Error('Could not get removed comments')
     })
-    .catch(() => { throw new Error('Could not get removed comments') })
-}
